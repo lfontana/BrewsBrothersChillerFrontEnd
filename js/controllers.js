285366661cc04244c75a64b8c44b4f39f93467a4
eparkhurst <eli.parkhurst@gmail.com>
2016-02-09 10:30:29 -0700

    Doesn't Work

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,12 @@
 app.controller('HomeController', function($scope){
   $scope.greeting = 'Welcome Brews Brothers'
 })
+app.controller('LoginController', function($scope){
+  $scope.place = 'login'
+})
+app.controller('BatchController', function($scope){
+  $scope.place = 'Batch View'
+})
+app.controller('NewBrewController', function($scope){
+  $scope.place = 'New Brew'
+})
