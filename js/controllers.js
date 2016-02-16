987aea6cc06cddfec3055ff28f31f2454f470a41
David-Shibley <davidjshibley@gmail.com>
2016-02-16 16:00:03 -0700

    added post route on new batch

--- js/controllers.js
+++ js/controllers.js
@@ -3,18 +3,19 @@ app.controller('mainController', function($scope, $http) {
     $scope.brews = data.data;
     console.log($scope.brews);
   });
-
+  $scope.user_id = 1;
+});
 app.controller('HomeController', function($scope, $http){
   $scope.greeting = 'Welcome Brews Brothers';
   $scope.singleBrew = false;
   $scope.showBatch = function(batch) {
     $scope.singleBrew = true;
     $scope.batch = batch;
-  }
+  };
   $scope.clear = function() {
     $scope.singleBrew = false;
-  }
-})
+  };
+});
 app.controller('LoginController', function($scope, $anchorScroll, $location){
   $scope.place = 'Login'
   $scope.toAbout = function() {
@@ -25,80 +26,86 @@ app.controller('LoginController', function($scope, $anchorScroll, $location){
 // app.controller('BatchController', function($scope, $stateParams){
 //   $scope.place = 'Batch View';
 // })
-app.controller('NewBrewController', function($scope){
+app.controller('NewBrewController', function($scope, $http){
   clearBrew()
   $scope.place = 'New Brew'
   $scope.submitBatch = function(){
     $scope.brew.created = new Date();
     $scope.brew.lastRun = $scope.brew.created;
+    $scope.brew.user_id = $scope.user_id;
     console.log($scope.brew);
     $scope.brews.push($scope.brew)
+    // $http.post('https://chillerdb.herokuapp.com/batch', $scope.brew, config).then(successCallback, errorCallback);
+    $http.post('localhost:3000/dashboard', $scope.brew).then(successCallback, errorCallback);
     clearBrew();
   }
+
   $scope.setStyle = function(style){
+    var time = new Date();
     $scope.brew.style = style;
-    // if (style === "Ale") {
-    //   $scope.brew.schedule = [
-    //         {
-    //         time: 0,
-    //         temp: 68
-    //         },
-    //         {
-    //         time: 86400,
-    //         temp: 65
-    //         },
-    //         {
-    //         time: 604800,
-    //         temp: 50
-    //         }
-    //       ];
-    // } else if (style === "Stout") {
-    //   $scope.brew.schedule = [
-    //         {
-    //         time: 0,
-    //         temp: 68
-    //         },
-    //         {
-    //         time: 76400,
-    //         temp: 75
-    //         },
-    //         {
-    //         time: 504800,
-    //         temp: 60
-    //         }
-    //       ];
-    // } else if (style === "Porter") {
-    //   $scope.brew.schedule = [
-    //         {
-    //         time: 0,
-    //         temp: 58
-    //         },
-    //         {
-    //         time: 96400,
-    //         temp: 55
-    //         },
-    //         {
-    //         time: 704800,
-    //         temp: 60
-    //         }
-    //       ];
-    // } else if (style === "Lager") {
-    //   $scope.brew.schedule = [
-    //         {
-    //         time: 0,
-    //         temp: 58
-    //         },
-    //         {
-    //         time: 96400,
-    //         temp: 55
-    //         },
-    //         {
-    //         time: 704800,
-    //         temp: 60
-    //         }
-    //       ];
-    // };
-    $scope.brew.schedule = $scope.sampleBrews.schedule;
+    $scope.brew.schedule = [];
+    console.log($scope.brew);
+    if (style === "Ale") {
+      $scope.brew.schedule = [
+            {
+            time: time,
+            temp: 68
+            },
+            {
+            time: time,
+            temp: 65
+            },
+            {
+            time: time,
+            temp: 50
+            }
+          ];
+    } else if (style === "Stout") {
+      $scope.brew.schedule = [
+            {
+            time: 0,
+            temp: 68
+            },
+            {
+            time: 76400,
+            temp: 75
+            },
+            {
+            time: 504800,
+            temp: 60
+            }
+          ];
+    } else if (style === "Porter") {
+      $scope.brew.schedule = [
+            {
+            time: 0,
+            temp: 58
+            },
+            {
+            time: 96400,
+            temp: 55
+            },
+            {
+            time: 704800,
+            temp: 60
+            }
+          ];
+    } else if (style === "Lager") {
+      $scope.brew.schedule = [
+            {
+            time: 0,
+            temp: 58
+            },
+            {
+            time: 96400,
+            temp: 55
+            },
+            {
+            time: 704800,
+            temp: 60
+            }
+          ];
+    };
   };
   function clearBrew(){
     $scope.brew = {};
