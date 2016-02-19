4c048e63c047571dc70b35f9cf33d2bceb9c18e0
Lou Fontana <loufontana@gmail.com>
2016-02-18 19:08:03 -0700

    front end and server working

--- js/controllers.js
+++ js/controllers.js
@@ -19,6 +19,7 @@ app.controller('mainController', function($scope, $http, $localStorage, $locatio
 app.controller('HomeController', function($scope, $http, batch_service){
 
   batch_service.getBatches().then(function(data){
+    console.log(data);  
     $scope.brews=data;
   })
 
